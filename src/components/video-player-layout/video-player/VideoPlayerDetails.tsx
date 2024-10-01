4dfd1c951ea55a03f1960b5ebcf8df3fa91e4004
JuanValeraDev <juanvaleradev@gmail.com>
2024-10-01 20:01:48 +0200

    Fixed infinite like button

--- src/components/video-player-layout/video-player/VideoPlayerDetails.tsx
+++ src/components/video-player-layout/video-player/VideoPlayerDetails.tsx
@@ -3,11 +3,24 @@
 import {Button} from "@/components/ui/button"
 import {ThumbsUp, Eye} from "lucide-react"
 import {VideoTypeProps} from '@/types/VideoType'
+import {useEffect, useState} from "react";
 
 
-const VideoPlayerDetails= ({video, onIncrementLikes}: VideoTypeProps & {
+const VideoPlayerDetails = ({video, onIncrementLikes}: VideoTypeProps & {
     onIncrementLikes: (id: string) => void
 }) => {
+
+    const [isLiked, setIsLiked] = useState(false)
+
+    useEffect(() => {
+        setIsLiked(localStorage.getItem(`isLiked_${video.id}`) === 'true')
+    }, [video.id])
+
+    const toggleIsLiked = () => {
+        setIsLiked(true)
+        localStorage.setItem(`isLiked_${video.id}`, String(true))
+    }
+
     return (
         <div className="space-y-4">
             <h1 className="text-2xl font-bold mb-4">{video.title}</h1>
@@ -24,16 +37,32 @@ const VideoPlayerDetails= ({video, onIncrementLikes}: VideoTypeProps & {
                             className="text-muted-foreground">{video.likes_count.toLocaleString()} likes</span>
                     </div>
                 </div>
-                <Button
-                    variant="outline"
-                    size="sm"
-                    onClick={() => onIncrementLikes(video.id)}
-                    className="flex items-center"
-                    aria-label="Like this video"
-                >
-                    <ThumbsUp className="h-4 w-4 mr-2"/>
-                    Like
-                </Button>
+                {
+                    !isLiked ?
+                        <Button
+                            variant="outline"
+                            size="sm"
+                            onClick={() => {
+                                onIncrementLikes(video.id)
+                                toggleIsLiked()
+                            }}
+                            className="flex items-center"
+                            aria-label="Like this video"
+                        >
+                            <ThumbsUp className="h-4 w-4 mr-2"/>
+                            Like
+                        </Button> : <Button
+                            variant="pressed"
+                            size="sm"
+                            className="flex items-center"
+                            aria-label="This video is already liked"
+                            aria-pressed={true}
+                        >
+                            <ThumbsUp className="h-4 w-4 mr-2"/>
+                            Liked
+                        </Button>
+
+                }
             </div>
             <p className="text-muted-foreground">{video.description}</p>
         </div>
