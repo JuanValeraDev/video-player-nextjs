64a8b8ea11c9e2aa8560ae5e420288a001582dcb
JuanValeraDev <juanvaleradev@gmail.com>
2024-10-01 16:43:39 +0200

    Allowing dinamically origin subdomains from Vercel

--- src/app/api/trpc/[trcp]/route.ts
+++ src/app/api/trpc/[trcp]/route.ts
@@ -1,14 +1,24 @@
 // src/app/api/trpc/[trpc]/route.ts
-import {fetchRequestHandler} from '@trpc/server/adapters/fetch';
-import {appRouter} from '@/server';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from '@/server';
 
 const handler = async (request: Request) => {
     // Definir los headers CORS comunes
-    const corsHeaders = {
-        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_API_ORIGIN || '*',
+    const corsHeaders: Record<string, string> = {
         'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type',
     };
+
+    // Permitir orígenes dinámicamente para subdominios de Vercel
+    const vercelSubdomain = /\.vercel\.app$/;
+    const origin = request.headers.get('origin');
+
+    // Si el origen de la solicitud coincide con el patrón de subdominio de Vercel
+    if (origin?.match(vercelSubdomain)) {
+        corsHeaders['Access-Control-Allow-Origin'] = origin; // Permitir el origen específico
+    } else {
+        corsHeaders['Access-Control-Allow-Origin'] = '*'; // O permitir todos los orígenes (puedes ajustar esto según tus necesidades)
+    }
 
     // Manejar las peticiones OPTIONS para CORS
     if (request.method === 'OPTIONS') {
@@ -36,6 +46,6 @@ const handler = async (request: Request) => {
     });
 };
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
 
-export const runtime = "edge"
+export const runtime = "edge";
