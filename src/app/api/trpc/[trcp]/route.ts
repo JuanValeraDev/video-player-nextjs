afa3937e63ec36fea659d3630385defdb3510eb0
JuanValeraDev <juanvaleradev@gmail.com>
2024-10-02 17:12:59 +0200

    Added request.method === options in route.ts

--- src/app/api/trpc/[trcp]/route.ts
+++ src/app/api/trpc/[trcp]/route.ts
@@ -3,6 +3,18 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/server';
 
 const handler = async (request: Request) => {
+    if (request.method === 'OPTIONS') {
+        return new Response(null, {
+            status: 204,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
+                'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                'Access-Control-Allow-Credentials': 'true',
+            },
+        });
+    }
+
     // Manejar las peticiones GET y POST para tRPC
     const response = await fetchRequestHandler({
         endpoint: '/api/trpc',
@@ -15,7 +27,9 @@ const handler = async (request: Request) => {
     return new Response(response.body, {
         ...response,
         headers: {
-            ...response.headers
+            ...response.headers,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true',
         },
     });
 };
