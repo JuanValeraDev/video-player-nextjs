f3f40584a334280f7370e56449abbbebf8c23c21
JuanValeraDev <juanvaleradev@gmail.com>
2024-10-02 17:36:15 +0200

    Changed the url in Access-Control-Allow-Origin

--- src/app/api/trpc/[trcp]/route.ts
+++ src/app/api/trpc/[trcp]/route.ts
@@ -7,7 +7,7 @@ const handler = async (request: Request) => {
         return new Response(null, {
             status: 204,
             headers: {
-                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': 'https://video-player-nextjs-ten.vercel.app/',
                 'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
                 'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
                 'Access-Control-Allow-Credentials': 'true',
