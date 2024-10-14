aa501d29bede43c70b6ab988ae79b5a125679621
JuanValeraDev <juanvaleradev@gmail.com>
2024-10-14 16:49:33 +0200

    Changed search inputs

--- src/components/home-layout/HomeLayout.tsx
+++ src/components/home-layout/HomeLayout.tsx
@@ -4,6 +4,8 @@ import Link from "next/link"
 import HomeLayoutBodySkeleton from "@/components/loading/HomeLayoutBodySkeleton"
 import HomeLayoutBody from "@/components/home-layout/HomeLayoutBody"
 import Footer from "@/components/Footer"
+import {Input} from "@/components/ui/input";
+import {Search} from "lucide-react";
 
 /* TODO
     BUGS:
@@ -43,24 +45,31 @@ export default function Home() {
                 }}>
                     <h1 className="text-2xl font-bold my-2 sm:my-0">Video Player</h1>
                 </Link>
-                <input
-                    type="text"
-                    placeholder="Search videos..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className="p-2 border rounded text-secondary-foreground my-2 sm:my-0"
-                    style={{ color: 'hsl(var(--input-foreground))'}}
-                />
-                <Button onClick={toggleDarkMode} className={"my-2 sm:my-0"}>
-                    {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-                </Button>
+                <div className="flex-row flex gap-3">
+                    <div className="relative max-w-sm mx-auto self-center">
+                        <Input
+                            type="search"
+                            placeholder="Search..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className="pl-10 pr-4 py-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                            aria-label="Search"
+                        />
+                        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5"
+                                aria-hidden="true"/>
+                    </div>
+
+                    <Button onClick={toggleDarkMode} className={"my-2 sm:my-0"}>
+                        {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+                    </Button>
+                </div>
             </header>
             <div className="flex flex-col max-h-dvh p-0">
                 <Suspense fallback={<HomeLayoutBodySkeleton/>}>
                     <HomeLayoutBody searchTerm={searchTerm}/>
                 </Suspense>
             </div>
-         <Footer/>
+            <Footer/>
         </>
     )
 }
--- src/components/video-player-layout/video-player-list/VideoList.tsx
+++ src/components/video-player-layout/video-player-list/VideoList.tsx
@@ -5,6 +5,8 @@ import {CardTitle} from "@/components/ui/card"
 import {VideoType} from "@/types/VideoType"
 import VideoListSkeleton from "@/components/loading/VideoListSkeleton"
 import VideoItemCard from "@/components/VideoItemCard"
+import {Input} from "@/components/ui/input";
+import {Search} from "lucide-react";
 
 const VideoList = ({videos, videoPlaying, onChangeVideoPlaying, onIncrementLikes, onIncrementWatches}: {
     videos: VideoType[],
@@ -26,14 +28,18 @@ const VideoList = ({videos, videoPlaying, onChangeVideoPlaying, onIncrementLikes
         <div className="flex flex-col max-h-dvh p-0 bg-secondary">
             <div className="sticky top-0 bg-secondary z-10 p-4 flex justify-between items-center  flex-col">
                 <CardTitle className="text-2xl font-bold mb-4">List of Videos</CardTitle>
-                <input
-                    type="text"
-                    placeholder="Search videos..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className="p-2 border rounded mb-4"
-                    style={{color: 'hsl(var(--input-foreground))'}}
-                />
+                <div className="relative max-w-sm mx-auto self-center">
+                    <Input
+                        type="search"
+                        placeholder="Search..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="pl-10 pr-4 py-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+                        aria-label="Search"
+                    />
+                    <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5"
+                            aria-hidden="true"/>
+                </div>
             </div>
             <div
                 className="flex-grow overflow-y-auto  p-4 flex flex-row lg:flex lg:flex-col gap-4 bg-secondary rounded-lg lg:self-center">
